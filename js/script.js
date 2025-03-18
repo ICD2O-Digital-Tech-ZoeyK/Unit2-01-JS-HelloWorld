71051185bc59f2461a5c148d92985180bec01ddc
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-03-18 14:59:28 +0000

    coded my first webpage with JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("hello-world").innerHTML = "This was coded with javascript by Zoey!"
+}
